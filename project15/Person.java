af06d5f2937ff43fff84d2e95b38b779e2422abc
Mirajul <mirajulislamanik100@gmail.com>
2024-10-04 23:11:14 +0600

    set get mathods

--- project15/Person.java
+++ project15/Person.java
@@ -1,32 +1,37 @@
 package project15;
 
 public class Person {
-    private String name,gender;
+    private String name, gender;
     private int age;
-    public void setName(String name)
-    {
+
+    public Person(String name, String gender, int age) {
         this.name = name;
-    }
-    public String getName()
-    {
-        return name;
-    }
-    public void setAge(int age)
-    {
+        this.gender = gender;
         this.age = age;
     }
-    public int getAge()
-    {
-        return age;
-    }
-    public void serGender(String gender)
-    {
-        this.gender = gender;
-    }
-    public String getGender()
-    {
-        return gender;
+
+    // public void setName(String name) {
+    // this.name = name;
+    // }
+
+    public String getName() {
+        return name;
     }
 
+    // public void setAge(int age) {
+    // this.age = age;
+    // }
+
+    public int getAge() {
+        return age;
+    }
+
+    // public void serGender(String gender) {
+    // this.gender = gender;
+    // }
+
+    public String getGender() {
+        return gender;
+    }
 
 }
--- project15/SetGetMathod.java
+++ project15/SetGetMathod.java
@@ -8,16 +8,18 @@ public class SetGetMathod {
         // Encapsulation
 
         Scanner input = new Scanner(System.in);
-        String name;
+        String name, gender;
+        int age;
         System.out.print("Enter your name : ");
         name = input.nextLine();
-        Person person = new Person();
-        person.setName(name);
-        person.setAge(20);
-        person.serGender("male");
-        System.out.println("name is : "+person.getName());
-        System.out.println("Age is "+person.getAge());
-        System.out.println("Gender is "+person.getGender());
+        System.out.print("Enter your gender : ");
+        gender = input.nextLine();
+        System.out.print("Enter your age : ");
+        age = input.nextInt();
+        Person person = new Person(name, gender, age);
+        System.out.println("name is : " + person.getName());
+        System.out.println("Age is " + person.getAge());
+        System.out.println("Gender is " + person.getGender());
 
     }
 }
